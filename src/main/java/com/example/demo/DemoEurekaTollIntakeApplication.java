a228ce1088ed2f9aed9704ef4a8c15399c1c6ad1
jerry691 <jerrywang6886@gmail.com>
2020-07-20 09:41:07 -0400

    initial commit

---
+++ src/main/java/com/example/demo/DemoEurekaTollIntakeApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
+@SpringBootApplication
+public class DemoEurekaTollIntakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEurekaTollIntakeApplication.class, args);
+	}
+
+	@StreamListener(Sink.INPUT)
+	public void log(String msg) {
+		System.out.println(msg);
+	}
+
+}
